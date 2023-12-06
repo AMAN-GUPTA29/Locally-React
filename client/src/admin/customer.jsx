ca3907a2bdd68ed94fb7ca22bc0878de9e6b093a
AMAN-GUPTA29 <aman.g21@iiits.in>
2023-12-06 23:53:06 +0530

    redux

--- client/src/admin/customer.jsx
+++ client/src/admin/customer.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import './admincss/customer.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./admincss/customer.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import Navbar from './navbar';
-
+import Navbar from "./navbar";
 
 const Customer = () => {
   const data = [
-    { _id: '1', name: 'John Doe', email: 'john@example.com' },
-    { _id: '2', name: 'Jane Doe', email: 'jane@example.com' },
-    { _id: '3', name: 'Bob Smith', email: 'bob@example.com' },
+    { _id: "1", name: "John Doe", email: "john@example.com" },
+    { _id: "2", name: "Jane Doe", email: "jane@example.com" },
+    { _id: "3", name: "Bob Smith", email: "bob@example.com" },
   ];
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <h1 className="head text-center fw-bold my-4">Customers Of Locally</h1>
       <div className="container d-flex justify-content-center">
         <table className="table-bluprint table table-hover m-1 p-3 rounded-3">
@@ -60,19 +59,38 @@ const Customer = () => {
 
       {/* View modal */}
       {data.map((user, i) => (
-        <div className="modal fade inf" id={`viewModalrow${i}`} key={i} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div
+          className="modal fade inf"
+          id={`viewModalrow${i}`}
+          key={i}
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
           <div className="modal-dialog modal-lg modal-dialog-centered">
             <div className="modal-content">
               <div className="modal-header">
                 <div className="modal-title fs-5" id="exampleModalLabel">
                   <h1>User Details</h1>
                 </div>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
               </div>
               <div className="modal-body">
-                <p><span className='viewmodal-names'>ID : </span> {user._id}</p>
-                <p><span className='viewmodal-names'>Name : </span> Mr.{user.name}</p>
-                <p><span className='viewmodal-names'>Email : </span> {user.email}</p>
+                <p>
+                  <span className="viewmodal-names">ID : </span> {user._id}
+                </p>
+                <p>
+                  <span className="viewmodal-names">Name : </span> Mr.
+                  {user.name}
+                </p>
+                <p>
+                  <span className="viewmodal-names">Email : </span> {user.email}
+                </p>
                 {/* Add other details as needed */}
               </div>
             </div>
@@ -82,24 +100,43 @@ const Customer = () => {
 
       {/* Delete modal */}
       {data.map((user, i) => (
-        <div className="modal fade inf" id={`deleteModalrow${i}`} key={i} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div
+          className="modal fade inf"
+          id={`deleteModalrow${i}`}
+          key={i}
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
           <div className="modal-dialog modal-dialog-centered">
             <div className="modal-content">
               <div className="modal-header">
                 <div className="modal-title fs-5" id="exampleModalLabel">
                   <h1>Block User</h1>
                 </div>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
               </div>
               <div className="modal-body fs-3">
                 {user && (
                   <div>
                     <p>Are you sure you want to block this user {user.name}?</p>
                     <div className="modal-footer">
-                      <button type="button" className="btn btn-outline-success px-5 py-2" data-bs-dismiss="modal">
+                      <button
+                        type="button"
+                        className="btn btn-outline-success px-5 py-2"
+                        data-bs-dismiss="modal"
+                      >
                         No
                       </button>
-                      <a href={`/customer/delete/${user._id}`} className="btn btn-outline-danger px-5 py-2">
+                      <a
+                        href={`/customer/delete/${user._id}`}
+                        className="btn btn-outline-danger px-5 py-2"
+                      >
                         Yes
                       </a>
                     </div>
@@ -108,7 +145,11 @@ const Customer = () => {
               </div>
             </div>
           </div>
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+            crossOrigin="anonymous"
+          ></script>
         </div>
       ))}
     </div>
--- client/src/pranav-component/sellerprofile/_Header.jsx
+++ client/src/pranav-component/sellerprofile/_Header.jsx
@@ -1,33 +1,41 @@
 import React from "react";
 
 export default ({ url_id, name, tag, like, dislike }) => {
-    return (
-        <header classname="header" id="home">
-            <div classname="container">
-                <div classname="infos">
-                    <h6 classname="subtitle">Hello,I'm</h6>
-                    <h6 classname="title">{name}</h6>
-                    <p>{tag}</p>
-                    <p>Likes : {like}</p>
-                    <p>Dislikes : {dislike}</p>
+  return (
+    <header classname="header" id="home">
+      <div classname="container">
+        <div classname="infos">
+          <h6 classname="subtitle">Hello,I'm</h6>
+          <h6 classname="title">{name}</h6>
+          <p>{tag}</p>
+          <p>Likes : {like}</p>
+          <p>Dislikes : {dislike}</p>
 
-                    <div classname="buttons pt-3">
-                        <a href={`/payment/${url_id}`}> <button classname="btn btn-primary rounded">HIRE
-                            ME</button></a>
+          <div classname="buttons pt-3">
+            {/* <div> */}{" "}
+            <button classname="btn btn-primary rounded">HIRE ME</button>
+            {/* </div> */}
+          </div>
 
-                    </div>
-
-                    <div classname="socials mt-4">
-                        <a classname="social-item" href="javascript:void(0)"><i classname="ti-facebook"></i></a>
-                        <a classname="social-item" href="javascript:void(0)"><i classname="ti-google"></i></a>
-                        <a classname="social-item" href="javascript:void(0)"><i classname="ti-github"></i></a>
-                        <a classname="social-item" href="javascript:void(0)"><i classname="ti-twitter"></i></a>
-                    </div>
-                </div>
-                <div classname="img-holder">
-                    <img src="/images/man.svg" alt="" />
-                </div>
-            </div>
-        </header>
-    )
-}
+          <div classname="socials mt-4">
+            <a classname="social-item" href="javascript:void(0)">
+              <i classname="ti-facebook"></i>
+            </a>
+            <a classname="social-item" href="javascript:void(0)">
+              <i classname="ti-google"></i>
+            </a>
+            <a classname="social-item" href="javascript:void(0)">
+              <i classname="ti-github"></i>
+            </a>
+            <a classname="social-item" href="javascript:void(0)">
+              <i classname="ti-twitter"></i>
+            </a>
+          </div>
+        </div>
+        <div classname="img-holder">
+          <img src="/images/man.svg" alt="" />
+        </div>
+      </div>
+    </header>
+  );
+};
