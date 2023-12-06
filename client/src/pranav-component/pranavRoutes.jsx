26f6039352b8d7aa3f22ada54e35cf590d0f4ec2
percyps2401 <onlystudies2424@gmail.com>
2023-12-07 01:09:24 +0530

    added history

--- client/src/pranav-component/pranavRoutes.jsx
+++ client/src/pranav-component/pranavRoutes.jsx
@@ -2,12 +2,16 @@ import About from "./aboutus/About";
 import ContactUs from "./contactus/ContactUs";
 import Display from "./sellerprofile/Display";
 import Customerrequest  from "./customer_request/Customerrequest";
+import History from "./History/History";
 
 export default [
     { path: "/about", element: <About /> },
     {path :"/contact" , element:<ContactUs/>},
     {path : "/customerView/display", element: <Display />},
     {path :"/customerrequest" , element:<Customerrequest/>},
+    {path :"/history" , element:<History/>},
+
+
     
     
 ]
