dba4c148259dd77ba9e7f5e6a7a0c1b962259be3
percyps2401 <onlystudies2424@gmail.com>
2023-12-06 18:56:17 +0530

    added seller profile page

--- client/src/pranav-component/pranavRoutes.jsx
+++ client/src/pranav-component/pranavRoutes.jsx
@@ -1,7 +1,10 @@
 import About from "./aboutus/About";
 import ContactUs from "./contactus/ContactUs";
+import Display from "./sellerprofile/Display";
 
 export default [
     { path: "/about", element: <About /> },
-    {path :"/contact" , element:<ContactUs/>}
+    {path :"/contact" , element:<ContactUs/>},
+    {path : "/customerView/display", element: <Display />},
+    
 ]
