72211d62d74b4fd1ff784e2990c43447a1fdf7bc
chaitanya-199 <chaitunaidu9347@gmail.com>
2024-02-22 00:59:13 +0530

    Seller profile page

--- client/src/chaitanya-component/chaitanyaRoutes.jsx
+++ client/src/chaitanya-component/chaitanyaRoutes.jsx
@@ -4,8 +4,10 @@ import { Services } from './Services';
 import { Layout } from './Layout';
 import { AddService } from './AddService';
 import { Footer } from './Footer';
+import SellerProfile from './SellerProfile';
 
 export default [
     { path: "/seller/services", element: <Services /> },
-    { path: "/addservices", element: <AddService/> },
+    { path: "/addservices", element: <AddService /> },
+    { path: "/seller/profile", element: <SellerProfile /> }
 ]
