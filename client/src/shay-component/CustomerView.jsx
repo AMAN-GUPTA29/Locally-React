a029c6d86f8985d9cb40a6cfe50cc331e13d6cfd
AMAN-GUPTA29 <aman.g21@iiits.in>
2024-02-23 12:30:42 +0530

    l

--- client/src/shay-component/CustomerView.jsx
+++ client/src/shay-component/CustomerView.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Footer from "./Footer";
 import NavPostLog from "./NavPostLog";
 import Body from "./_CustomerView";
-
+import Fotterfinal from "../pages/landingpage/footer";
 
 export default () => {
-    return (
-        <div>
-            <NavPostLog />
-            <Body />
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div>
+      <NavPostLog />
+      <Body />
+      <Fotterfinal />
+    </div>
+  );
+};
